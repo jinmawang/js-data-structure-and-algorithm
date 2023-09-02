1f162806d594b75426c2c045afa2a0de3ca73011
jawesome <jawesome@163.com>
2023-09-02 18:38:48 +0800

    feat: 修改部分代码

--- 排序/1-冒泡排序.js
+++ 排序/1-冒泡排序.js
@@ -9,37 +9,35 @@
 // https://www.cnblogs.com/echolun/p/12638903.html
 // https://segmentfault.com/a/1190000014175918
 
-const arr1 = [5,2,1,3,6,4];
+const arr1 = [5, 2, 1, 3, 6, 4];
 // 交换一个数组中的两个元素
- function swap(arr,index1,index2) {
-     const temp = arr[index1];
-     arr[index1] = arr[index2];
-     arr[index2] = temp; 
- }
+function swap(arr, index1, index2) {
+  const temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
+}
 
-
-
- // 冒泡排序
- function bubbleSort(arr) {
-     // 《数据结构与算法》的解法 有点难理解
-    // for(let outer = arr.length; outer >= 2 ; -- outer) {
-    //     for(let inner = 0;inner <= outer-1; ++inner) {
-    //         if(arr[inner]>arr[inner+1]){
-    //             swap(arr,inner,inner+1)
-    //         }
-    //     }
-    // }
-    // 路人解法
-    // 内层循环是遍历数组，两两对调，遍历结束后当前最大的移动到数组的末尾
-    // 外层循环是遍历次数（每遍历一次相当去将当前最大的移动到数组的末尾）
-    for (var j = 0; j < arr.length - 1; j++) {
-        // 注意这里 -j;  https://segmentfault.com/a/1190000014175918
-        // 内层循环不用每次都遍历到数组最后，只有第一次需要，j代表当前的遍历次数，每遍历一次，内存循环的遍历就少一个长度
-        for(let i = 0,l = arr.length; i<l-1-j; i++) {
-            if(arr[i]>arr[i+1]){
-              swap(arr,i,i+1)
-            }
-         }
+// 冒泡排序
+function bubbleSort(arr) {
+  // 《数据结构与算法》的解法 有点难理解
+  // for(let outer = arr.length; outer >= 2 ; -- outer) {
+  //     for(let inner = 0;inner <= outer-1; ++inner) {
+  //         if(arr[inner]>arr[inner+1]){
+  //             swap(arr,inner,inner+1)
+  //         }
+  //     }
+  // }
+  // 路人解法
+  // 内层循环是遍历数组，两两对调，遍历结束后当前最大的移动到数组的末尾
+  // 外层循环是遍历次数（每遍历一次相当去将当前最大的移动到数组的末尾）
+  for (let j = 0; j < arr.length - 1; j++) {
+    // 注意这里 -j;  https://segmentfault.com/a/1190000014175918
+    // 内层循环不用每次都遍历到数组最后，只有第一次需要，j代表当前的遍历次数，每遍历一次，内存循环的遍历就少一个长度
+    for (let i = 0; i < arr.length - 1 - j; i++) {
+      if (arr[i] > arr[i + 1]) {
+        swap(arr, i, i + 1);
+      }
     }
- }
- bubbleSort(arr1)
+  }
+}
+bubbleSort(arr1);
--- 排序/2-选择排序.js
+++ 排序/2-选择排序.js
@@ -2,36 +2,34 @@
 // 检查完所有元素后，最小的元素会被放到数组的第一个位置，然后算法会从 第二个位置继续。
 // 这个过程一直进行，当进行到数组的倒数第二个位置时，所有的数据便 完成了排序。
 
-
 // 选择排序会用到嵌套循环。外循环从数组的第一个元素移动到倒数第二个元素;
 // 内循环从第 二个数组元素移动到最后一个元素，查找比当前外循环所指向的元素小的元素。
 // 每次内循环 迭代后，数组中最小的值都会被赋值到合适的位置。
 
-const arr1 = [3,6,4,2,1,5];
+const arr1 = [3, 6, 4, 2, 1, 5];
 
- // 交换一个数组中的两个元素
- function swap(arr,index1,index2) {
-    const temp = arr[index1];
-    arr[index1] = arr[index2];
-    arr[index2] = temp; 
+// 交换一个数组中的两个元素
+function swap(arr, index1, index2) {
+  const temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
 }
 // 内层循环是遍历找出当前的数组，找出最小值，和当前的项对调
 // 外层循环是控制遍历次数
 function selectionSort(arr) {
-    for (let j = 0; j < arr.length; j++) {
-        // 每次遍历被比较的项，第一次先假设数组第一个就是最小的
-        let min = arr[j];
-        let minIndex = j;
-        // 依次对调后，从j+1项开始遍历
-        for (let i = j+1; i < arr.length; i++) {
-            if(arr[i]<min) {
-                min = arr[i];
-                minIndex = i;
-            }
-        }
-       swap(arr,j,minIndex)
+  for (let j = 0; j < arr.length - 1; j++) {
+    // 每次遍历被比较的项，第一次先假设数组第一个就是最小的
+    let min = arr[j];
+    let minIndex = j;
+    // 从j+1项开始遍历,寻找本轮循环中最小的值
+    for (let i = j + 1; i < arr.length; i++) {
+      if (arr[i] < min) {
+        min = arr[i];
+        minIndex = i;
+      }
     }
-   
+    swap(arr, j, minIndex);
+  }
 }
 selectionSort(arr1);
 // 备注下，第一天看冒泡排序，第二天看选择排序
